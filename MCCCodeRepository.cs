94904bcf8a1f29a88555c5de231ea97643de6fc7
arnabtec <56453061+arnabtec@users.noreply.github.com>
2020-04-23 21:14:52 -0500

    Add files via upload

---
+++ MCCCodeRepository.cs
@@ -0,0 +1,61 @@
+﻿using MerchantDataAPI.Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MerchantDataAPI.DataAccess
+{
+    public class MCCCodeRepository : IMCCCodeRepository
+    {
+        private readonly IMongoCollection<MerchantCategoryCode> _merchantCodes;
+
+        public MCCCodeRepository(IMerchantDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            var collection = settings.CollectionName;
+
+            _merchantCodes = database.GetCollection<MerchantCategoryCode>(collection);
+        }
+
+        public async Task<List<MerchantCategoryCode>> SearchMCCByCode(string mccCode)
+        {
+            return await _merchantCodes.Find<MerchantCategoryCode>(merchantcategorycode => merchantcategorycode.MCCCode == mccCode).ToListAsync();
+        }
+
+        public async Task<List<MerchantCategoryCode>> SearchMCCByDescription(string mccdescription)
+        {
+            var filter = new BsonDocument(){
+                            {"$or", new BsonArray(){
+                                    new BsonDocument(){{"MCCCode", new BsonRegularExpression(mccdescription, "i")}},
+                                    new BsonDocument(){{"MCCName", new BsonRegularExpression(mccdescription, "i")}},
+                                    new BsonDocument(){{"Keywords", new BsonRegularExpression(mccdescription, "i")}},
+                                    new BsonDocument(){{"MCCDescription", new BsonRegularExpression(mccdescription, "i")} }}}
+                            };
+
+            return await _merchantCodes.Find<MerchantCategoryCode>(filter).ToListAsync();
+        }
+
+        public async Task<List<MerchantCategoryCode>> SearchMCCByDescriptionAndType(string mccdescription, string transProcType)
+        {
+            var filter = new BsonDocument(){
+                            {"$and", new BsonArray(){new BsonDocument(){
+                                {"$or", new BsonArray(){
+                                    new BsonDocument(){{"MCCCode", new BsonRegularExpression(mccdescription, "i")}},
+                                    new BsonDocument(){{"MCCName", new BsonRegularExpression(mccdescription, "i")}},
+                                    new BsonDocument(){{"Keywords", new BsonRegularExpression(mccdescription, "i")}},
+                                    new BsonDocument(){{"MCCDescription", new BsonRegularExpression(mccdescription, "i")} }}}
+                            },new BsonDocument(){{"MarkedforB2B", transProcType } }}} };
+
+            return await _merchantCodes.Find<MerchantCategoryCode>(filter).ToListAsync();
+        }
+
+        public async Task<List<MerchantCategoryCode>> SearchMCCByType(string transProcType)
+        {
+            return await _merchantCodes.Find<MerchantCategoryCode>(merchantcategorycode => merchantcategorycode.MarkedforB2B == transProcType).ToListAsync();
+        }
+    }
+}
